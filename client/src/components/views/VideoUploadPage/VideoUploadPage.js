020151afa5c4e719d86cd8b41d83736229db489e
KimJoungUk <dnrl24a@gmail.com>
2020-03-10 19:01:29 +0900

    upload-mp4_file

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon} from 'antd'
 import Dropzone from "react-dropzone"
+import Axios from 'axios';
 
 const { TextArea } = Input  
 const { Title } = Typography;
@@ -38,7 +39,24 @@ const VideoUploadPage = () => {
   const onCategoryChange = e => {
     setCategory(e.currentTarget.value)
   }
+  
+  const onDrop = (files) => {
+    console.log(files)
+    let formData = new FormData;
+    const config = {
+      header: {'content-type': 'multipart/form-data'}
+    }
+    formData.append("file", files[0])
 
+    Axios.post('/api/video/uploadfiles', formData, config)
+      .then(response => {
+        if(response.data.success) {
+          console.log(response.data)
+        } else {
+          alert('비디오 업로드를 실패했습니다.')
+        }
+      })
+  }
   return (
     <div style={{maxWidth:'700px', margin:'2rem auto'}}>
       <div style={{textAlign:'center', marginBottom:'2rem'}}>
@@ -48,9 +66,10 @@ const VideoUploadPage = () => {
       <Form onSubmit>
         <div style={{ display:'flex', justifyConten:'space-between' }}>
           <Dropzone 
-            onDrop
-            multiple
-            maxSize >
+            onDrop={onDrop}
+            multiple={false}
+            maxSize={100000000} 
+          >
               {({ getRootProps, getInputProps }) => (
                 <div style={{width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
                 alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
