d390951488012f354aa6b8be3871f0fe2a177e03
KimJoungUk <dnrl24a@gmail.com>
2020-03-11 16:31:45 +0900

    videoInfo upload to mongoDB

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon} from 'antd'
 import Dropzone from "react-dropzone"
 import Axios from 'axios';
+import { useSelector } from 'react-redux'
 
 const { TextArea } = Input  
 const { Title } = Typography;
@@ -17,8 +18,9 @@ const CategoryOptions = [
   { value:2, label: "Music"},
   { value:3, label: "Pets & Animals"}
 ]
-const VideoUploadPage = () => {
+const VideoUploadPage = (props) => {
 
+  const user = useSelector(state => state.user)
   const [VideoTitle, setVideoTitle] = useState('')
   const [Description, setDescription] = useState('')
   const [Private, setPrivate] = useState(0)
@@ -77,13 +79,41 @@ const VideoUploadPage = () => {
         }
       })
   }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let variable = {
+      writer: user.userData._id,
+      title: VideoTitle,
+      description: Description,
+      privacy: Private,
+      filePath: FilePath,
+      category: Category,
+      duration: Duration,
+      thumbnail: ThumbnailPath,
+    }
+
+    Axios.post('/api/video/uploadVideo', variable)
+    .then(response => {
+      if(response.data.success) {
+        message.success('성공적으로 업로드 하였습니다.')
+        setTimeout(() => {
+          props.history.push('/')
+        }, 2000)
+      } else {
+        alert('비디오 업로드에 실패하였습니다.')
+      }
+    })
+  }
+
   return (
     <div style={{maxWidth:'700px', margin:'2rem auto'}}>
       <div style={{textAlign:'center', marginBottom:'2rem'}}>
         <Title level={2}>Upload Video</Title>
       </div>
 
-      <Form onSubmit>
+      <Form onSubmit={onSubmit}>
         <div style={{ display:'flex', justifyContent:'space-between' }}>
           <Dropzone 
             onDrop={onDrop}
@@ -144,7 +174,7 @@ const VideoUploadPage = () => {
         <br />
         <br />
         
-        <Button type='primary' size='large' onClick>
+        <Button type='primary' size='large' onClick={onSubmit}>
           Submit
         </Button>
 
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require('multer')
@@ -27,6 +27,15 @@ const upload = multer({ storage: storage }).single("file")
 //=================================
 //             Video
 //=================================
+
+// 비디오 정보를 저장한다.
+router.post('/uploadVideo', (req, res) => {
+  const video = new Video(req.body)
+  video.save((err, doc) => {
+    if(err) return res.json({ success: false, err })
+    res.status(200).json({ success: true })
+  })
+})
 
 router.post('/uploadfiles', (req, res) => {
   // 비디오를 서버에 저장하면 된다
