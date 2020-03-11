fbe1fb3a0973f989e4ae0cb6f2ebbf6936e98123
KimJoungUk <dnrl24a@gmail.com>
2020-03-11 18:51:05 +0900

    get selet one video view

--- server/routes/video.js
+++ server/routes/video.js
@@ -47,6 +47,15 @@ router.post('/uploadfiles', (req, res) => {
   })
 })
 
+router.post('/getVideoDetail', (req, res) => {
+  Video.findOne({ "_id": req.body.videoId })
+  .populate('writer')
+  .exec((err, videoDetail) => {
+    if(err) return res.status(400).send(err)
+    return res.status(200).json({ success: true, videoDetail })
+  })
+})
+
 router.get('/getVideos', (req, res) => {
   // 비디오를 DB에서 가져와서 클라이언트에 보낸다
   Video.find()
